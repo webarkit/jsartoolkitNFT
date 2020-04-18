15169fe11fed4322dc8b2211d91068fa363fd8ba
Walter Perdan <github@kalwaltart.it>
2020-04-18 14:21:57 +0200

    version 0.4.1 in constructor

--- src/arnft-constructor.js
+++ src/arnft-constructor.js
@@ -4,6 +4,6 @@ var ARnft = function (width, height, config) {
   this.root = new THREE.Object3D();
   this.root.matrixAutoUpdate = false;
   this.config = config;
-  this.version = '0.3.0';
+  this.version = '0.4.1';
   console.log('ARnft ', this.version);
 };
