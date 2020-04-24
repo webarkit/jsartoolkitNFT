fc4b68541f22013e53ee2bae84722aa99e032142
Walter Perdan <github@kalwaltart.it>
2020-04-25 00:15:51 +0200

    version 0.6.4 - removing from pkg from github

--- js/artoolkitNFT.api.js
+++ js/artoolkitNFT.api.js
@@ -76,7 +76,7 @@
         this._bwpointer = undefined;
         this._lumaCtx = undefined;
 
-        this.version = '0.6.3';
+        this.version = '0.6.4';
         console.info('JsartoolkitNFT ', this.version);
 
         if (typeof cameraPara === 'string') {
