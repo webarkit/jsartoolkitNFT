2ded339e4f6067c3aa1f28b2141cb28d8123a932
Walter Perdan <github@kalwaltart.it>
2020-10-26 19:08:06 +0100

    new version 0.7.3

--- js/artoolkitNFT.api.js
+++ js/artoolkitNFT.api.js
@@ -76,7 +76,7 @@
         this._bwpointer = undefined;
         this._lumaCtx = undefined;
 
-        this.version = '0.7.2';
+        this.version = '0.7.3';
         console.info('JsartoolkitNFT ', this.version);
 
         if (typeof cameraPara === 'string') {
