cd4f5555d0db742f31109e7df1de5517019352d8
Walter Perdan <github@kalwaltart.it>
2020-10-29 17:55:10 +0100

    removing canvas

--- src/ARControllerNFT.js
+++ src/ARControllerNFT.js
@@ -1,5 +1,4 @@
 import ARToolkitNFT from './ARToolkitNFT'
-import { createCanvas } from 'canvas'
 
 export default class ARControllerNFT {
   constructor (width, height, cameraParam, options) {
@@ -55,9 +54,6 @@ export default class ARControllerNFT {
     if (this.options.canvas) {
       // in case you use Node.js, create a canvas with node-canvas
       this.canvas = this.options.canvas
-    } else if (this.options.canvas == null) {
-      this.canvas = createCanvas(this.videoWidth, this.videoHeight)
-      console.debug('canvas created with node-canvas');
     } else if (typeof document !== 'undefined') {
       // try creating a canvas from document
       this.canvas = document.createElement('canvas')
