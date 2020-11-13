f46b331c56ded8523e2e264b845302d6eff89617
Walter Perdan <info@kalwaltart.it>
2020-11-13 17:01:52 +0100

    rebuilt libs with 0.8.0 version

--- js/artoolkitNFT.api.js
+++ js/artoolkitNFT.api.js
@@ -76,7 +76,7 @@
         this._bwpointer = undefined;
         this._lumaCtx = undefined;
 
-        this.version = '0.7.5';
+        this.version = '0.8.0';
         console.info('JsartoolkitNFT ', this.version);
 
         if (typeof cameraPara === 'string') {
--- src/ARToolkitNFT.js
+++ src/ARToolkitNFT.js
@@ -14,7 +14,7 @@ export default class ARToolkitNFT {
     this.instance
     this.markerNFTCount = 0
     this.cameraCount = 0
-    this.version = '0.7.5'
+    this.version = '0.8.0'
     console.info('ARToolkitNFT ', this.version)
   }
 
