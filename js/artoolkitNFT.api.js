eb37035c47a78f4259317d348e491ad02a2ddaf7
kalwalt <github@kalwaltart.it>
2021-03-22 13:40:00 +0100

    build libs with latest Emscripten 2.0.15

--- js/artoolkitNFT.api.js
+++ js/artoolkitNFT.api.js
@@ -76,7 +76,7 @@
         this._bwpointer = undefined;
         this._lumaCtx = undefined;
 
-        this.version = '0.8.2';
+        this.version = '0.9.0';
         console.info('JsartoolkitNFT ', this.version);
 
         if (typeof cameraPara === 'string') {
