c1ce60e7fa2cbe9e16da9d5ecb4818e4bcdd6825
kalwalt <info@kalwaltart.it>
2021-10-12 23:29:44 +0200

    rebuild libs with 0.9.4 version

--- js/artoolkitNFT.api.js
+++ js/artoolkitNFT.api.js
@@ -76,7 +76,7 @@
         this._bwpointer = undefined;
         this._lumaCtx = undefined;
 
-        this.version = '0.9.3';
+        this.version = '0.9.4';
         console.info('JsartoolkitNFT ', this.version);
 
         if (typeof cameraPara === 'string') {
