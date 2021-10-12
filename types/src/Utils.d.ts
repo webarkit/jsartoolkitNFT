67bfa79562751fab4d4b6382d178554899b7edf2
kalwalt <info@kalwaltart.it>
2021-10-12 23:42:27 +0200

    small fixes and new 0.9.4 version

--- types/src/Utils.d.ts
+++ types/src/Utils.d.ts
@@ -1,5 +1,5 @@
 export default class Utils {
     static fetchRemoteData(url: string): Promise<Uint8Array>;
-    static fetchRemoteDataCallback(url: string, callback: any): Promise<void>;
+    static fetchRemoteDataCallback(url: string, callback: any): Promise<any>;
     static string2Uint8Data(string: string): Uint8Array;
 }
