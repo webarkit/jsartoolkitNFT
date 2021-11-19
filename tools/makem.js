c61cbecf0fd77b7cb56cba99f7420e635d221199
Squareys <squareys@googlemail.com>
2021-11-19 14:22:07 +0100

    Cleanup and remove excessive whitespace between compile flags
    Signed-off-by: Squareys <squareys@googlemail.com>

--- tools/makem.js
+++ tools/makem.js
@@ -6,11 +6,11 @@
 
 
 var
-	exec = require('child_process').exec,
-	path = require('path'),
-	fs = require('fs'),
-	os = require('os'),
-	child;
+exec = require('child_process').exec,
+    path = require('path'),
+    fs = require('fs'),
+    os = require('os'),
+    child;
 
 const platform = os.platform();
 
@@ -21,10 +21,10 @@ var WITH_FILTERING = 0;
 var arguments = process.argv;
 
 for (var j = 2; j < arguments.length; j++) {
-	if (arguments[j] == '--no-libar') {
-		NO_LIBAR = true;
-		console.log('Building jsartoolkitNFT with --no-libar option, libar will be preserved.');
-	};
+    if (arguments[j] == '--no-libar') {
+        NO_LIBAR = true;
+        console.log('Building jsartoolkitNFT with --no-libar option, libar will be preserved.');
+    };
 }
 
 var HAVE_NFT = 1;
@@ -33,8 +33,8 @@ var EMSCRIPTEN_ROOT = process.env.EMSCRIPTEN;
 var WEBARKITLIB_ROOT = process.env.WEBARKITLIB_ROOT || path.resolve(__dirname, "../emscripten/WebARKitLib");
 
 if (!EMSCRIPTEN_ROOT) {
-  console.log("\nWarning: EMSCRIPTEN environment variable not found.")
-  console.log("If you get a \"command not found\" error,\ndo `source <path to emsdk>/emsdk_env.sh` and try again.");
+    console.log("\nWarning: EMSCRIPTEN environment variable not found.")
+    console.log("If you get a \"command not found\" error,\ndo `source <path to emsdk>/emsdk_env.sh` and try again.");
 }
 
 var EMCC = EMSCRIPTEN_ROOT ? path.resolve(EMSCRIPTEN_ROOT, 'emcc') : 'emcc';
@@ -52,22 +52,22 @@ var BUILD_WASM_ES6_FILE = 'artoolkitNFT_ES6_wasm.js';
 var BUILD_MIN_FILE = 'artoolkitNFT.min.js';
 
 var MAIN_SOURCES = [
-	'ARToolKitJS.cpp',
-	'trackingMod.c',
-	'trackingMod2d.c',
+    'ARToolKitJS.cpp',
+    'trackingMod.c',
+    'trackingMod2d.c',
 ];
 
 if (!fs.existsSync(path.resolve(WEBARKITLIB_ROOT, 'include/AR/config.h'))) {
-	console.log("Renaming and moving config.h.in to config.h");
-	fs.copyFileSync(
-		path.resolve(WEBARKITLIB_ROOT, 'include/AR/config.h.in'),
-		path.resolve(WEBARKITLIB_ROOT, 'include/AR/config.h')
-	);
-	console.log("Done!");
+    console.log("Renaming and moving config.h.in to config.h");
+    fs.copyFileSync(
+        path.resolve(WEBARKITLIB_ROOT, 'include/AR/config.h.in'),
+        path.resolve(WEBARKITLIB_ROOT, 'include/AR/config.h')
+    );
+    console.log("Done!");
 }
 
 MAIN_SOURCES = MAIN_SOURCES.map(function(src) {
-  return path.resolve(SOURCE_PATH, src);
+    return path.resolve(SOURCE_PATH, src);
 }).join(' ');
 
 let srcTest = path.resolve(__dirname, WEBARKITLIB_ROOT + '/lib/SRC/');
@@ -75,36 +75,36 @@ let srcTest = path.resolve(__dirname, WEBARKITLIB_ROOT + '/lib/SRC/');
 let arSources, ar_sources;
 
 if (platform === 'win32') {
-	var glob = require("glob");
-function match(pattern) {
-    var r = glob.sync('emscripten/WebARKitLib/lib/SRC/' + pattern);
-    return r;
-}
-function matchAll(patterns, prefix="") {
-    let r = [];
-    for(let pattern of patterns) {
-        r.push(...(match(prefix + pattern)));
+    var glob = require("glob");
+    function match(pattern) {
+        var r = glob.sync('emscripten/WebARKitLib/lib/SRC/' + pattern);
+        return r;
     }
-    return r;
-}
+    function matchAll(patterns, prefix="") {
+        let r = [];
+        for(let pattern of patterns) {
+            r.push(...(match(prefix + pattern)));
+        }
+        return r;
+    }
 
-  ar_sources = matchAll([
-    'AR/arLabelingSub/*.c',
-    'AR/*.c',
-    'ARICP/*.c',
-    'ARUtil/log.c',
-    'ARUtil/file_utils.c',
-]);
+    ar_sources = matchAll([
+        'AR/arLabelingSub/*.c',
+        'AR/*.c',
+        'ARICP/*.c',
+        'ARUtil/log.c',
+        'ARUtil/file_utils.c',
+    ]);
 } else {
-	ar_sources = [
-	  'AR/arLabelingSub/*.c',
-	  'AR/*.c',
-	  'ARICP/*.c',
-	  'ARUtil/log.c',
-	  'ARUtil/file_utils.c',
-	].map(function(src) {
-		return path.resolve(__dirname, WEBARKITLIB_ROOT + '/lib/SRC/', src);
-	});
+    ar_sources = [
+        'AR/arLabelingSub/*.c',
+        'AR/*.c',
+        'ARICP/*.c',
+        'ARUtil/log.c',
+        'ARUtil/file_utils.c',
+    ].map(function(src) {
+        return path.resolve(__dirname, WEBARKITLIB_ROOT + '/lib/SRC/', src);
+    });
 }
 
 var ar2_sources = [
@@ -125,73 +125,73 @@ var ar2_sources = [
     'coord.c',
     'util.c',
 ].map(function(src) {
-	return path.resolve(__dirname, WEBARKITLIB_ROOT + '/lib/SRC/AR2/', src);
+    return path.resolve(__dirname, WEBARKITLIB_ROOT + '/lib/SRC/AR2/', src);
 });
 
 var kpm_sources = [
-	'kpmHandle.cpp',
-	'kpmRefDataSet.cpp',
-	'kpmMatching.cpp',
-	'kpmResult.cpp',
-	'kpmUtil.cpp',
-	'kpmFopen.c',
-	'FreakMatcher/detectors/DoG_scale_invariant_detector.cpp',
-	'FreakMatcher/detectors/gaussian_scale_space_pyramid.cpp',
-	'FreakMatcher/detectors/gradients.cpp',
-	'FreakMatcher/detectors/harris.cpp',
-	'FreakMatcher/detectors/orientation_assignment.cpp',
-	'FreakMatcher/detectors/pyramid.cpp',
-	'FreakMatcher/facade/visual_database_facade.cpp',
-	'FreakMatcher/matchers/hough_similarity_voting.cpp',
-	'FreakMatcher/matchers/freak.cpp',
-	'FreakMatcher/framework/date_time.cpp',
-	'FreakMatcher/framework/image.cpp',
-	'FreakMatcher/framework/logger.cpp',
-	'FreakMatcher/framework/timers.cpp',
+    'kpmHandle.cpp',
+    'kpmRefDataSet.cpp',
+    'kpmMatching.cpp',
+    'kpmResult.cpp',
+    'kpmUtil.cpp',
+    'kpmFopen.c',
+    'FreakMatcher/detectors/DoG_scale_invariant_detector.cpp',
+    'FreakMatcher/detectors/gaussian_scale_space_pyramid.cpp',
+    'FreakMatcher/detectors/gradients.cpp',
+    'FreakMatcher/detectors/harris.cpp',
+    'FreakMatcher/detectors/orientation_assignment.cpp',
+    'FreakMatcher/detectors/pyramid.cpp',
+    'FreakMatcher/facade/visual_database_facade.cpp',
+    'FreakMatcher/matchers/hough_similarity_voting.cpp',
+    'FreakMatcher/matchers/freak.cpp',
+    'FreakMatcher/framework/date_time.cpp',
+    'FreakMatcher/framework/image.cpp',
+    'FreakMatcher/framework/logger.cpp',
+    'FreakMatcher/framework/timers.cpp',
 ].map(function(src) {
-	return path.resolve(__dirname, WEBARKITLIB_ROOT + '/lib/SRC/KPM/', src);
+    return path.resolve(__dirname, WEBARKITLIB_ROOT + '/lib/SRC/KPM/', src);
 });
 
 if (HAVE_NFT) {
-  ar_sources = ar_sources
-  .concat(ar2_sources)
-  .concat(kpm_sources);
+    ar_sources = ar_sources
+        .concat(ar2_sources)
+        .concat(kpm_sources);
 }
 
 var DEFINES = ' ';
-if (HAVE_NFT) DEFINES += ' -D HAVE_NFT ';
-if (WITH_FILTERING) DEFINES += ' -D WITH_FILTERING ';
+if (HAVE_NFT) DEFINES += ' -D HAVE_NFT';
+if (WITH_FILTERING) DEFINES += ' -D WITH_FILTERING';
 
 var FLAGS = '' + OPTIMIZE_FLAGS;
-FLAGS += ' -Wno-warn-absolute-paths ';
+FLAGS += ' -Wno-warn-absolute-paths';
 FLAGS += ' -s MINIMAL_RUNTIME=1';
 FLAGS += ' -s TOTAL_MEMORY=' + MEM + ' ';
 FLAGS += ' -s USE_ZLIB=1';
-FLAGS += ' -s USE_LIBJPEG';
-FLAGS += ' --memory-init-file 0 '; // for memless file
-FLAGS += " -s EXPORTED_RUNTIME_METHODS='[\"FS\"]'"
+FLAGS += ' -s USE_LIBJPEG=1';
+FLAGS += ' --memory-init-file 0'; // for memless file
+FLAGS += " -s EXPORTED_RUNTIME_METHODS=[\"FS\"]"
 FLAGS += ' -s ALLOW_MEMORY_GROWTH=1';
 
 var WASM_FLAGS = ' -s SINGLE_FILE=1 -msimd128'
-var ES6_FLAGS = ' -s EXPORT_ES6=1 -s USE_ES6_IMPORT_META=0 -s MODULARIZE=1 ';
+var ES6_FLAGS = ' -s EXPORT_ES6=1 -s USE_ES6_IMPORT_META=0 -s MODULARIZE=1';
 
-var PRE_FLAGS = ' --pre-js ' + path.resolve(__dirname, '../js/artoolkitNFT.api.js') +' ';
+var PRE_FLAGS = ' --pre-js ' + path.resolve(__dirname, '../js/artoolkitNFT.api.js');
 
 FLAGS += ' --bind ';
 
 /* DEBUG FLAGS */
-var DEBUG_FLAGS = ' -g ';
-DEBUG_FLAGS += ' -s ASSERTIONS=1 '
-DEBUG_FLAGS += ' --profiling '
+var DEBUG_FLAGS = ' -g';
+DEBUG_FLAGS += ' -s ASSERTIONS=1'
+DEBUG_FLAGS += ' --profiling'
 DEBUG_FLAGS += ' -s ALLOW_MEMORY_GROWTH=1';
-DEBUG_FLAGS += '  -s DEMANGLE_SUPPORT=1 ';
+DEBUG_FLAGS += '  -s DEMANGLE_SUPPORT=1';
 
-var INCLUDES = [
+var INCLUDES = ' ' + ([
     path.resolve(__dirname, WEBARKITLIB_ROOT + '/include'),
     OUTPUT_PATH,
     SOURCE_PATH,
     path.resolve(__dirname, WEBARKITLIB_ROOT + '/lib/SRC/KPM/FreakMatcher'),
-].map(function(s) { return '-I' + s }).join(' ');
+].map(function(s) { return '-I' + s }).join(' '));
 
 function format(str) {
     for (var f = 1; f < arguments.length; f++) {
@@ -209,46 +209,46 @@ function clean_builds() {
 
     try {
         var files = fs.readdirSync(OUTPUT_PATH);
-				var filesLength = files.length;
+        var filesLength = files.length;
         if (filesLength > 0)
-				if (NO_LIBAR == true){
-					filesLength -= 1;
-				}
-            for (var i = 0; i < filesLength; i++) {
-                var filePath = OUTPUT_PATH + '/' + files[i];
-                if (fs.statSync(filePath).isFile())
-                    fs.unlinkSync(filePath);
+            if (NO_LIBAR == true){
+                filesLength -= 1;
             }
+        for (var i = 0; i < filesLength; i++) {
+            var filePath = OUTPUT_PATH + '/' + files[i];
+            if (fs.statSync(filePath).isFile())
+                fs.unlinkSync(filePath);
+        }
     }
     catch(e) { return console.log(e); }
 }
 
-var compile_arlib = format(EMCC + ' ' + INCLUDES + ' '
-    + ar_sources.join(' ')
-    + FLAGS + ' ' + DEFINES + ' -r -o {OUTPUT_PATH}libar.bc ',
+var compile_arlib = format(EMCC + INCLUDES
+    + ' ' + ar_sources.join(' ')
+    + FLAGS + ' ' + DEFINES + ' -r -o {OUTPUT_PATH}libar.bc',
     OUTPUT_PATH);
 
-var ALL_BC = " {OUTPUT_PATH}libar.bc ";
+var ALL_BC = ' {OUTPUT_PATH}libar.bc';
 
-var compile_combine = format(EMCC + ' ' + INCLUDES + ' '
-    + ALL_BC + MAIN_SOURCES
-    + FLAGS + ' -s WASM=0' + ' '  + DEBUG_FLAGS + DEFINES + ' -o {OUTPUT_PATH}{BUILD_FILE} ',
+var compile_combine = format(EMCC + INCLUDES
+    + ALL_BC + ' ' + MAIN_SOURCES
+    + FLAGS + ' -s WASM=0' + ' '  + DEBUG_FLAGS + DEFINES + ' -o {OUTPUT_PATH}{BUILD_FILE}',
     OUTPUT_PATH, OUTPUT_PATH, BUILD_DEBUG_FILE);
 
-var compile_combine_min = format(EMCC + ' ' + INCLUDES + ' '
-    + ALL_BC + MAIN_SOURCES
-    + FLAGS + ' -s WASM=0' + ' ' + DEFINES + PRE_FLAGS + ' -o {OUTPUT_PATH}{BUILD_FILE} ',
+var compile_combine_min = format(EMCC + INCLUDES
+    + ALL_BC + ' ' + MAIN_SOURCES
+    + FLAGS + ' -s WASM=0' + ' ' + DEFINES + PRE_FLAGS + ' -o {OUTPUT_PATH}{BUILD_FILE}',
     OUTPUT_PATH, OUTPUT_PATH, BUILD_MIN_FILE);
 
-var compile_wasm = format(EMCC + ' ' + INCLUDES + ' '
-    + ALL_BC + MAIN_SOURCES
-    + FLAGS + WASM_FLAGS + DEFINES + PRE_FLAGS + ' -o {OUTPUT_PATH}{BUILD_FILE} ',
+var compile_wasm = format(EMCC + INCLUDES
+    + ALL_BC + ' ' + MAIN_SOURCES
+    + FLAGS + WASM_FLAGS + DEFINES + PRE_FLAGS + ' -o {OUTPUT_PATH}{BUILD_FILE}',
     OUTPUT_PATH, OUTPUT_PATH, BUILD_WASM_FILE);
 
-var compile_wasm_es6 = format(EMCC + ' ' + INCLUDES + ' '
-		 + ALL_BC + MAIN_SOURCES
-		 + FLAGS + WASM_FLAGS + DEFINES + ES6_FLAGS + ' -o {OUTPUT_PATH}{BUILD_FILE} ',
-		 OUTPUT_PATH, OUTPUT_PATH, BUILD_WASM_ES6_FILE);
+var compile_wasm_es6 = format(EMCC + INCLUDES
+    + ALL_BC + ' ' + MAIN_SOURCES
+    + FLAGS + WASM_FLAGS + DEFINES + ES6_FLAGS + ' -o {OUTPUT_PATH}{BUILD_FILE}',
+    OUTPUT_PATH, OUTPUT_PATH, BUILD_WASM_ES6_FILE);
 
 /*
  * Run commands
@@ -296,7 +296,7 @@ addJob(compile_wasm_es6)
 addJob(compile_combine_min);
 
 if (NO_LIBAR == true){
-  jobs.splice(1,1);
+    jobs.splice(1,1);
 }
 
 runJob();
