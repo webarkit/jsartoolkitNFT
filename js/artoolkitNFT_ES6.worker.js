b416c80f3f1e4557a924b737b19ab305ac3a9f4d
kalwalt <github@kalwaltart.it>
2022-01-16 23:03:33 +0100

    removing grunt jshint - upgrading packages - format with prettier

--- js/artoolkitNFT_ES6.worker.js
+++ js/artoolkitNFT_ES6.worker.js
@@ -1,71 +1,80 @@
-importScripts('../dist/ARToolkitNFT.js')
+importScripts("../dist/ARToolkitNFT.js");
 
 self.onmessage = function (e) {
-  var msg = e.data
+  var msg = e.data;
   switch (msg.type) {
-    case 'load': {
-      load(msg)
-      return
+    case "load": {
+      load(msg);
+      return;
     }
-    case 'process': {
-      next = msg.imagedata
-      process()
+    case "process": {
+      next = msg.imagedata;
+      process();
     }
   }
-}
+};
 
-var next = null
-var ar = null
-var markerResult = null
-var marker
+var next = null;
+var ar = null;
+var markerResult = null;
+var marker;
 
-function load (msg) {
-  console.debug('Loading marker at: ', msg.marker)
+function load(msg) {
+  console.debug("Loading marker at: ", msg.marker);
 
   var onLoad = function (arController) {
-    ar = arController
-    var cameraMatrix = ar.getCameraMatrix()
+    ar = arController;
+    var cameraMatrix = ar.getCameraMatrix();
 
-    ar.addEventListener('getNFTMarker', function (ev) {
-      markerResult = { type: 'found', matrixGL_RH: JSON.stringify(ev.data.matrixGL_RH)}
-    })
+    ar.addEventListener("getNFTMarker", function (ev) {
+      markerResult = {
+        type: "found",
+        matrixGL_RH: JSON.stringify(ev.data.matrixGL_RH),
+      };
+    });
 
     ar.loadNFTMarker(msg.marker, function (id) {
-      ar.trackNFTMarkerId(id)     
-      let marker = ar.getNFTData(ar.id, 0)
-      console.log("nftMarker data: ", marker)
-      postMessage({type: 'markerInfos', marker: marker})
-      console.log('loadNFTMarker -> ', id)
-      postMessage({ type: 'endLoading', end: true })
+      ar.trackNFTMarkerId(id);
+      let marker = ar.getNFTData(ar.id, 0);
+      console.log("nftMarker data: ", marker);
+      postMessage({ type: "markerInfos", marker: marker });
+      console.log("loadNFTMarker -> ", id);
+      postMessage({ type: "endLoading", end: true });
     }).catch(function (err) {
-      console.log('Error in loading marker on Worker', err)
-    })
+      console.log("Error in loading marker on Worker", err);
+    });
 
-    postMessage({ type: 'loaded', proj: JSON.stringify(cameraMatrix) })
-  }
+    postMessage({ type: "loaded", proj: JSON.stringify(cameraMatrix) });
+  };
 
   var onError = function (error) {
-    console.error(error)
-  }
+    console.error(error);
+  };
 
-  console.debug('Loading camera at:', msg.camera_para)
+  console.debug("Loading camera at:", msg.camera_para);
 
   // we cannot pass the entire ARControllerNFT, so we re-create one inside the Worker, starting from camera_param
-  ARToolkitNFT.ARControllerNFT.initWithDimensions(msg.pw, msg.ph, msg.camera_para).then(onLoad).catch(onError)
+  ARToolkitNFT.ARControllerNFT.initWithDimensions(
+    msg.pw,
+    msg.ph,
+    msg.camera_para
+  )
+    .then(onLoad)
+    .catch(onError);
 }
 
-function process () {
-  markerResult = null
+function process() {
+  markerResult = null;
 
   if (ar && ar.process) {
-    ar.process(next)
+    ar.process(next);
   }
 
   if (markerResult) {
-    postMessage(markerResult)
+    postMessage(markerResult);
   } else {
-    postMessage({ type: 'not found' })
+    postMessage({ type: "not found" });
   }
 
-  next = null
+  next = null;
 }
