7f0689338670b8b692f55e3054506139ab32eba7
kalwalt <info@kalwaltart.it>
2022-11-12 21:16:51 +0100

    version 1.1.6

--- js/artoolkitNFT.api.js
+++ js/artoolkitNFT.api.js
@@ -61,7 +61,7 @@
         this._bwpointer = undefined;
         this._lumaCtx = undefined;
 
-        this.version = '1.1.5';
+        this.version = '1.1.6';
         console.info('JsartoolkitNFT ', this.version);
 
         if (typeof cameraPara === 'string') {
