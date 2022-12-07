02ad63c7bb664927c43dfa1b229a4bfe633d9e93
Walter Perdan <github@kalwaltart.it>
2022-12-07 22:56:27 +0100

    running prettier

--- src/abstractions/AbstractARControllerNFT.ts
+++ src/abstractions/AbstractARControllerNFT.ts
@@ -1,40 +1,59 @@
 import { IImageObj } from "./CommonInterfaces";
 export abstract class AbstractARControllerNFT {
-    static initWithDimensions: (width: number, height: number, cameraParam: string) => Promise<AbstractARControllerNFT>;
-    static initWithImage: (image: IImageObj, cameraParam: string) => Promise<AbstractARControllerNFT>;
-    process: (image: IImageObj) => void;
-    detectNFTMarker: () => void;
-    trackNFTMarkerId: (id: number, markerWidth?: number) => object;
-    detectMarker: (image: any) => number;
-    getNFTMarker: (markerIndex: number) => object;
-    getNFTData: (id: number, index: number) => object;
-    addEventListener: (name: string, callback: object) => void;
-    removeEventListener: (name: string, callback: object) => void;
-    dispatchEvent: (event: {
-        name: string;
-        target: any;
-        data?: object;
-    }) => void;
-    debugSetup: () => void;
-    transMatToGLMat: (transMat: Float64Array, glMat: Float64Array, scale?: number) => Float64Array;
-    arglCameraViewRHf: (glMatrix: Float64Array, glRhMatrix?: Float64Array, scale?: number) => Float64Array;
-    getTransformationMatrix: () => Float64Array;
-    getCameraMatrix: () => Float64Array;
-    setDebugMode: (mode: boolean) => number;
-    getDebugMode: () => boolean;
-    getProcessingImage: () => number;
-    setLogLevel: (mode: boolean) => number;
-    getLogLevel: () => number;
-    setProjectionNearPlane: (value: number) => void;
-    getProjectionNearPlane: () => number;
-    setProjectionFarPlane: (value: number) => void;
-    getProjectionFarPlane: () => number;
-    setThresholdMode: (mode: number) => number;
-    getThresholdMode: () => number;
-    setThreshold: (threshold: number) => number;
-    getThreshold: () => number;
-    loadNFTMarker: (urlOrData: string, onSuccess: (ids: number) => void, onError: (err: number) => void) => Promise<number[]>;
-    loadNFTMarkers: (urlOrData: Array<string>, onSuccess: (ids: number[]) => void, onError: (err: number) => void) => Promise<number[]>;
-    setImageProcMode: (mode: number) => number;
-    getImageProcMode: () => number;
+  static initWithDimensions: (
+    width: number,
+    height: number,
+    cameraParam: string
+  ) => Promise<AbstractARControllerNFT>;
+  static initWithImage: (
+    image: IImageObj,
+    cameraParam: string
+  ) => Promise<AbstractARControllerNFT>;
+  process: (image: IImageObj) => void;
+  detectNFTMarker: () => void;
+  trackNFTMarkerId: (id: number, markerWidth?: number) => object;
+  detectMarker: (image: any) => number;
+  getNFTMarker: (markerIndex: number) => object;
+  getNFTData: (id: number, index: number) => object;
+  addEventListener: (name: string, callback: object) => void;
+  removeEventListener: (name: string, callback: object) => void;
+  dispatchEvent: (event: { name: string; target: any; data?: object }) => void;
+  debugSetup: () => void;
+  transMatToGLMat: (
+    transMat: Float64Array,
+    glMat: Float64Array,
+    scale?: number
+  ) => Float64Array;
+  arglCameraViewRHf: (
+    glMatrix: Float64Array,
+    glRhMatrix?: Float64Array,
+    scale?: number
+  ) => Float64Array;
+  getTransformationMatrix: () => Float64Array;
+  getCameraMatrix: () => Float64Array;
+  setDebugMode: (mode: boolean) => number;
+  getDebugMode: () => boolean;
+  getProcessingImage: () => number;
+  setLogLevel: (mode: boolean) => number;
+  getLogLevel: () => number;
+  setProjectionNearPlane: (value: number) => void;
+  getProjectionNearPlane: () => number;
+  setProjectionFarPlane: (value: number) => void;
+  getProjectionFarPlane: () => number;
+  setThresholdMode: (mode: number) => number;
+  getThresholdMode: () => number;
+  setThreshold: (threshold: number) => number;
+  getThreshold: () => number;
+  loadNFTMarker: (
+    urlOrData: string,
+    onSuccess: (ids: number) => void,
+    onError: (err: number) => void
+  ) => Promise<number[]>;
+  loadNFTMarkers: (
+    urlOrData: Array<string>,
+    onSuccess: (ids: number[]) => void,
+    onError: (err: number) => void
+  ) => Promise<number[]>;
+  setImageProcMode: (mode: number) => number;
+  getImageProcMode: () => number;
 }
--- src/abstractions/CommonInterfaces.ts
+++ src/abstractions/CommonInterfaces.ts
@@ -1,22 +1,22 @@
 export interface IImageObj extends HTMLCanvasElement {
-    videoWidth: number;
-    width: number;
-    videoHeight: number;
-    height: number;
-    data: Uint8ClampedArray;
+  videoWidth: number;
+  width: number;
+  videoHeight: number;
+  height: number;
+  data: Uint8ClampedArray;
 }
 
 export interface INFTMarker {
-    inPrevious: boolean;
-    inCurrent: boolean;
-    matrix: Float64Array;
-    matrixGL_RH: Float64Array;
-    markerWidth: number;
+  inPrevious: boolean;
+  inCurrent: boolean;
+  matrix: Float64Array;
+  matrixGL_RH: Float64Array;
+  markerWidth: number;
 }
 
 export interface INFTMarkerInfo {
-    error: number;
-    found: number;
-    id: number;
-    pose: Float64Array;
-  }
+  error: number;
+  found: number;
+  id: number;
+  pose: Float64Array;
+}
