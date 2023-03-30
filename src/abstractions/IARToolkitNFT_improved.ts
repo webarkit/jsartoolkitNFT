1f0006cc082977b5723040a1473e1c608be4cfad
kalwalt <info@kalwaltart.it>
2023-03-30 20:39:22 +0200

    convert properties to methods in IARToolkitNFT - removing some not necessary properties in ARControllerNFT_improved

--- src/abstractions/IARToolkitNFT_improved.ts
+++ src/abstractions/IARToolkitNFT_improved.ts
@@ -1,45 +1,38 @@
 import { INFTMarkerInfo } from "./CommonInterfaces";
 export interface IARToolkitNFT {
-  getCameraLens: () => any;
-  passVideoData: (
+  getCameraLens(): any;
+  passVideoData(
     videoFrame: Uint8ClampedArray,
     videoLuma: Uint8Array
-  ) => void;
-  setup: {
-    (width: number, height: number, cameraId: number): number;
-  };
-  setupAR2: {
-    (): void;
-  };
-  setDebugMode: (mode: boolean) => number;
-  getDebugMode: () => boolean;
-  getProcessingImage: () => number;
-  setLogLevel: (mode: boolean) => number;
-  getLogLevel: () => number;
+  ): void;
+  setup(width: number, height: number, cameraId: number): number;
+  setupAR2(): void;
+  setDebugMode(mode: boolean): number;
+  getDebugMode(): boolean;
+  getProcessingImage(): number;
+  setLogLevel(mode: boolean): number;
+  getLogLevel(): number;
   NFTMarkerInfo: INFTMarkerInfo;
-  loadCamera: (cameraParam: string) => Promise<number>;
-  setProjectionNearPlane: {
-    (value: number): void;
-  };
-  getProjectionNearPlane: () => number;
-  setProjectionFarPlane: (value: number) => void;
-  getProjectionFarPlane: () => number;
-  setThresholdMode: (mode: number) => number;
-  getThresholdMode: () => number;
-  setThreshold: (threshold: number) => number;
-  getThreshold: () => number;
-  //_addNFTMarkers(vec: any): void;
-  addNFTMarkers: (
+  loadCamera(cameraParam: string): Promise<number>;
+  setProjectionNearPlane(value: number): void;
+  getProjectionNearPlane(): number;
+  setProjectionFarPlane(value: number): void;
+  getProjectionFarPlane(): number;
+  setThresholdMode(mode: number): number;
+  getThresholdMode(): number;
+  setThreshold(threshold: number): number;
+  getThreshold(): number;
+  addNFTMarkers(
     urls: Array<string>,
     callback: (ids: number[]) => void,
     onError2: (errorNumber: number) => void
-  ) => Array<number>;
-  detectMarker: () => number;
-  detectNFTMarker: () => number;
-  getNFTMarker: (markerIndex: number) => INFTMarkerInfo;
-  getNFTData: (index: number) => object;
-  setImageProcMode: (mode: number) => number;
-  getImageProcMode: () => number;
+  ): Array<number>;
+  detectMarker(): number;
+  detectNFTMarker(): number;
+  getNFTMarker(markerIndex: number): INFTMarkerInfo;
+  getNFTData(index: number): object;
+  setImageProcMode(mode: number): number;
+  getImageProcMode(): number;
   FS: any;
   StringList: any;
 }
