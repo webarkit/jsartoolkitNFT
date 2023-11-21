3f1922050ca49e626dcf8516a65db9ae3468d5e9
Walter Perdan <github@kalwaltart.it>
2023-11-22 00:37:50 +0100

    version 1.6.1 -fix for issue https://github.com/webarkit/jsartoolkitNFT/issues/335

--- js/artoolkitNFT_ES6.api.js
+++ js/artoolkitNFT_ES6.api.js
@@ -58,7 +58,7 @@ class ARControllerNFT {
         this._bwpointer = undefined;
         this._lumaCtx = undefined;
 
-        this.version = '1.6.0';
+        this.version = '1.6.1';
         console.info('JsartoolkitNFT ', this.version);
 
         if (typeof cameraPara === 'string') {
