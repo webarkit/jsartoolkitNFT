6aa84f835db701f57f5ca2dfee66efa480960e67
Walter Perdan <github@kalwaltart.it>
2024-11-22 00:13:16 +0100

    move Uint8ArrayToStr method in Utils class

--- types/src/Utils.d.ts
+++ types/src/Utils.d.ts
@@ -2,4 +2,5 @@ export default class Utils {
     static fetchRemoteData(url: string): Promise<Uint8Array>;
     static fetchRemoteDataCallback(url: string, callback: any): Promise<any>;
     static string2Uint8Data(string: string): Uint8Array;
+    static Uint8ArrayToStr(array: any): string;
 }
