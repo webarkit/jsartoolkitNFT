9d12383dfad2ab6203c800ed89e5968fdfc4b339
kalwalt <github@kalwaltart.it>
2025-03-07 17:46:44 +0100

    version 1.7.3

--- js/artoolkitNFT_ES6.api.js
+++ js/artoolkitNFT_ES6.api.js
@@ -58,7 +58,7 @@ class ARControllerNFT {
         this._bwpointer = undefined;
         this._lumaCtx = undefined;
 
-        this.version = '1.7.2';
+        this.version = '1.7.3';
         console.info('JsartoolkitNFT ', this.version);
 
         if (typeof cameraPara === 'string') {
