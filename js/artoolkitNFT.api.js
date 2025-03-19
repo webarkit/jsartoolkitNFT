05281c2a5f9da9586c07f6012c25cb9eb10d7f61
kalwalt <github@kalwaltart.it>
2025-03-19 17:37:13 +0100

    version 1.7.4

--- js/artoolkitNFT.api.js
+++ js/artoolkitNFT.api.js
@@ -63,7 +63,7 @@
         this._bwpointer = undefined;
         this._lumaCtx = undefined;
 
-        this.version = '1.7.3';
+        this.version = '1.7.4';
         console.info('JsartoolkitNFT ', this.version);
         console.warn("This library is deprecated, use the ES6 version instead!! It will be removed in a future release.")
 
