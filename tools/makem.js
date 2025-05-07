2b72c251aa7fd542cca5ee1c80223c2afd24eaf1
Walter Perdan <github@kalwaltart.it>
2025-05-07 16:38:45 +0200

    Enable Zlib support in build flags for improved compression

--- tools/makem.js
+++ tools/makem.js
@@ -219,7 +219,7 @@ FLAGS += " -Wno-warn-absolute-paths";
 FLAGS += " -Wno-return-type-c-linkage"; // Add this line to disable the warning
 FLAGS += " -s TOTAL_MEMORY=" + MEM + " ";
 FLAGS += " -s USE_LIBJPEG=1";
-//FLAGS += " -s USE_ZLIB=1";
+FLAGS += " -s USE_ZLIB=1";
 FLAGS += ' -s EXPORTED_RUNTIME_METHODS=["FS"]';
 FLAGS += " -s ALLOW_MEMORY_GROWTH=1";
 FLAGS += " --bind "; // Ensure --bind is included
